729c1541ea7ae08ae8db5a2c4937a9787a92c4b8
Serisha Kothapalli <77577361+ManiSerishaKothapalli@users.noreply.github.com>
2022-04-17 16:07:04 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # Netflix-UI
-
-
-This is the clone of landing page of netflix
-
-# Created a seperate folder for assets 
---
+++ script.js
@@ -0,0 +1,67 @@
+// OPEN QUESTION
+
+const answer1Div = document.getElementById("answer-1");
+const btn1 = document.getElementById("question-1");
+
+btn1.onclick = function () {
+  if (answer1Div.style.display == "block") {
+    answer1Div.style.display = "none";
+  } else {
+    answer1Div.style.display = "block";
+  }
+};
+
+const answer2Div = document.getElementById("answer-2");
+const btn2 = document.getElementById("question-2");
+
+btn2.onclick = function () {
+  if (answer2Div.style.display == "block") {
+    answer2Div.style.display = "none";
+  } else {
+    answer2Div.style.display = "block";
+  }
+};
+
+const answer3Div = document.getElementById("answer-3");
+const btn3 = document.getElementById("question-3");
+
+btn3.onclick = function () {
+  if (answer3Div.style.display == "block") {
+    answer3Div.style.display = "none";
+  } else {
+    answer3Div.style.display = "block";
+  }
+};
+
+const answer4Div = document.getElementById("answer-4");
+const btn4 = document.getElementById("question-4");
+
+btn4.onclick = function () {
+  if (answer4Div.style.display == "block") {
+    answer4Div.style.display = "none";
+  } else {
+    answer4Div.style.display = "block";
+  }
+};
+
+const answer5Div = document.getElementById("answer-5");
+const btn5 = document.getElementById("question-5");
+
+btn5.onclick = function () {
+  if (answer5Div.style.display == "block") {
+    answer5Div.style.display = "none";
+  } else {
+    answer5Div.style.display = "block";
+  }
+};
+
+const answer6Div = document.getElementById("answer-6");
+const btn6 = document.getElementById("question-6");
+
+btn6.onclick = function () {
+  if (answer6Div.style.display == "block") {
+    answer6Div.style.display = "none";
+  } else {
+    answer6Div.style.display = "block";
+  }
+};
